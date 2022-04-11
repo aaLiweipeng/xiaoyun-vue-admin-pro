29054aabc98bf298c2f01638828322b9eb2accd6
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-04-11 23:41:26 +0800

    完成表单数据绑定，提交时数据校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="login-container">
 
-        <el-form class="login-form">
+        <el-form class="login-form"
+          :model="loginForm"
+          :rules="loginRules">
+
             <div class="title-container">
                 <h3 class="title">用户登录</h3>
             </div>
 
             <!-- username -->
-            <el-form-item>
+            <el-form-item prop="username">
                 <!-- 图标 -->
                 <span class="svg-container">
                     <!-- <el-icon>
@@ -16,11 +19,16 @@
                     <svg-icon icon="user"></svg-icon>
                 </span>
                 <!-- 输入框 -->
-                <el-input placeholder="username" name="username" type="text"></el-input>
+                <el-input
+                  placeholder="username"
+                  name="username"
+                  type="text"
+                  v-model="loginForm.username"
+                ></el-input>
             </el-form-item>
 
             <!-- password -->
-            <el-form-item>
+            <el-form-item prop="password">
                 <!-- 图标 -->
                 <span class="svg-container">
                     <!-- <el-icon>
@@ -35,7 +43,9 @@
                 <!-- 输入框 -->
                 <el-input
                   placeholder="password"
-                   name="password"></el-input>
+                  name="password"
+                  v-model="loginForm.password"
+                ></el-input>
 
                 <span class="show-pwd">
                     <!-- <el-icon>
@@ -59,7 +69,31 @@
 // import { Avatar } from '@element-plus/icons'
 // SvgIcon 已经全局导入了，这里可以注释了
 // import SvgIcon from '../../../src/components/SvgIcon/index.vue'
-import {} from 'vue'
+import { ref } from 'vue'
+import { validatePassword } from './rules'
+
+// 数据源
+const loginForm = ref({
+  username: 'super-admin',
+  password: '123456'
+})
+// 验证规则
+const loginRules = ref({
+  username: [
+    {
+      required: true,
+      trigger: 'blur',
+      message: '用户名为必填项'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      trigger: 'blur',
+      validator: validatePassword()
+    }
+  ]
+})
 </script>
 
 <style lang="scss" scoped>
