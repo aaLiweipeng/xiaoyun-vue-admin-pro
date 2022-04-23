752de72f342f85ffb0eb018d76ec4d9dfc427048
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-04-24 02:56:57 +0800

    本地缓存token方案

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,22 @@
 import { login } from '@/api/sys'
 import md5 from 'md5'
+import { setItem, getItem } from '@/utils/storage'
+import { TOKEN } from '@/constant'
+
 export default {
   namespaced: true, // 表这是一个单独的模块
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    // 初始值：先从本地缓存中获取，没有的话再初始为空
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken (state, token) {
+      // 存VueX
+      state.token = token
+      // 存本地缓存
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     /**
      * 登录请求动作
@@ -21,6 +34,8 @@ export default {
           password: md5(password) // md5 加密的密码
         })
           .then(data => {
+            // 传mutations
+            this.commit('user/setToken', data.data.data.token)
             resolve(data)
           })
           .catch(err => {
