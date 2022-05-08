06f3246d9fe7274ef4b407d5de7b29398d777b0f
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-05-09 02:37:20 +0800

    css使用v-bind初试

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -4,7 +4,7 @@
     <!-- 左侧栏的header部分 -->
     <div class="logo-container">
       <el-avatar
-        size="44"
+        :size="logoHeight"
         shape="square"
         src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
       />
@@ -24,11 +24,13 @@
 <script setup>
 import SidebarMenu from './SidebarMenu'
 import {} from 'vue'
+
+const logoHeight = 44
 </script>
 
 <style lang="scss" scoped>
 .logo-container {
-  height: 44px;
+  height: v-bind(logoHeight) + 'px';
   padding: 10px 0 22px 0;
   display: flex;
   align-items: center;
