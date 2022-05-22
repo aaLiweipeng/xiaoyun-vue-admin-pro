5ff9196591a84cc9f2127fd4a89aeee75382830c
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-05-23 00:58:09 +0800

    引入element语言包和配置判断、引入项目自定义语言包

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,15 @@
+/**
+ * 注册element相关
+ */
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en'
+import store from '@/store'
 
-export default (app) => {
-  app.use(ElementPlus, { locale })
+export default app => {
+  app.use(ElementPlus, {
+    // 判断getters 中 全局配置lang字段是否为en，否则默认为中文
+    locale: store.getters.language === 'en' ? en : zhCn
+  })
 }
