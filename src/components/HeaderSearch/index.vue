8bbcab273343ca43123a571646856b0febdd213b
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-07-01 01:58:18 +0800

    优化headerSearch相关的bug
    1. 在 `search` 打开时，点击 `body界面的任何区域`，都可以关闭 `search`；
    2. 在 `search` 关闭时，清理 `searchOptions`
    3. 使得`headerSearch` 具备国际化能力

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -30,11 +30,12 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { filterRouters } from '@/utils/route'
 import { useRouter } from 'vue-router'
 import { generateRoutes } from './FuseData'
 import Fuse from 'fuse.js'
+import { watchSwitchLang } from '@/utils/i18n'
 
 // 控制 search 显示
 const isShow = ref(false)
@@ -43,7 +44,11 @@ const headerSearchSelectRef = ref(null)
 
 const onShowClick = () => {
   isShow.value = !isShow.value
-  headerSearchSelectRef.value.focus()
+  if (isShow.value) {
+    headerSearchSelectRef.value.focus()
+  } else {
+    onClose()
+  }
 }
 
 // search 相关【el-select的v-model】
@@ -67,7 +72,7 @@ const onSelectChange = val => {
 // 检索数据源
 const router = useRouter()
 // generateMenus返回菜单规则可用路由，且这里用computed做响应式变量
-const searchPool = computed(() => {
+let searchPool = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateRoutes(filterRoutes)
 })
@@ -75,26 +80,66 @@ console.log('searchPool.value --- ', searchPool.value)
 
 /**
   * 搜索库相关
+  * initFuse 接收一个搜索池数据源【searchPool】，初始化一个模糊搜索对象【fuse】
   * Fuse 一参：数据源 ； 二参：配置对象
   */
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进行排序
-  shouldSort: true,
-  // 匹配长度 超过这个值的 才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
-  // name：搜索的键
-  // weight：对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
+let fuse
+const initFuse = searchPool => {
+  fuse = new Fuse(searchPool, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度 超过这个值的 才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+initFuse(searchPool.value) // 初始化时调用一次
+
+// 处理国际化
+// 监听语言变化，重新计算数据源初始化 `fuse` 即可
+watchSwitchLang(() => {
+  searchPool = computed(() => {
+    // generateRoutes中有做回调；
+    // 国际化变量【store.getters.language】发生变化时，重新获取 菜单搜索池值【generateRoutes】
+    const filterRoutes = filterRouters(router.getRoutes())
+    return generateRoutes(filterRoutes)
+  })
+  // 重新初始化 模糊搜索实例
+  initFuse(searchPool.value)
+})
+
+/**
+ *
+ * 关闭 search 的处理事件
+ */
+const onClose = () => {
+  headerSearchSelectRef.value.blur() // 清除输入框焦点
+  isShow.value = false // 隐藏headerSearch
+  searchOptions.value = [] // 清除options待选项
+}
+/**
+ * 监听 headerSearch
+ * headerSearch打开时，给全局body添加click事件监听器
+ * click时候触发 close 回调
+ */
+watch(isShow, val => {
+  if (val) {
+    document.body.addEventListener('click', onClose)
+  } else {
+    document.body.removeEventListener('click', onClose)
+  }
 })
 </script>
 
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,4 +1,6 @@
 import i18n from '@/i18n'
+import { watch } from 'vue'
+import store from '@/store'
 
 /**
  * 语言包的属性值恰好跟定义路由时候的 title键一样
@@ -7,4 +9,21 @@ import i18n from '@/i18n'
  */
 export function generateTitle (title) {
   return i18n.global.t('msg.route.' + title)
+}
+
+/**
+ * 【观察者模式】
+ * 接收可拓展的回调函数 作为参数，
+ * 这里可以视为 被观察者，
+ * 调用这个函数的地方 可以视为 观察者；
+ * 当国际化变量发生变化时，回调这些“注册”到这里的 回调函数
+ * @param  {...any} cbs 可拓展的回调函数参数
+ */
+export function watchSwitchLang (...cbs) {
+  watch(
+    () => store.getters.language,
+    () => {
+      cbs.forEach(cb => cb(store.getters.language))
+    }
+  )
 }
